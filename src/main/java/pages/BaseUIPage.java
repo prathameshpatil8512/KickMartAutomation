1cbec7ee1c5e58abf562bcdad7fcd447d2ebbfb2
prathameshpatil8512 <prathameshpatil8512@yahoo.com>
2024-07-10 19:26:15 +0530

    updated
    added some classes and methods

--- src/main/java/pages/BaseUIPage.java
+++ src/main/java/pages/BaseUIPage.java
@@ -1,4 +1,84 @@
 package pages;
 
-public class BaseUIPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import utils.Webdriver;
+
+import java.sql.Driver;
+
+public class BaseUIPage extends Webdriver {
+
+    public BaseUIPage() {
+        super();
+    }
+
+    public static void clickElement(WebElement element) {
+        try {
+            if (element.isDisplayed()) {
+                element.click();
+                System.out.println("Element clicked");
+            } else {
+                System.out.println("Element not clickable");
+            }
+        } catch (Exception e) {
+            System.out.println("An error occured: "+e.getMessage());
+        }
+    }
+
+    public static void typeText(WebElement element, String text) {
+        try {
+            if (element.isDisplayed()) {
+                element.clear();
+                element.sendKeys(text);
+                System.out.println(text+" entered in text box.");
+            } else {
+                System.out.println("Text not entered");
+            }
+        } catch (Exception e) {
+            System.out.println("An error occured: "+e.getMessage());
+        }
+    }
+
+    public static void selectDropdownByVisibleText(WebElement element, String text) {
+        try {
+            if (element.isDisplayed()) {
+                Select dropdown = new Select(element);
+                dropdown.selectByVisibleText(text);
+                System.out.println("Dropdown value selected by visible text successfully.");
+            } else {
+                System.out.println("Dropdown is not interactable.");
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+
+    public static void selectDropdownByIndex(WebElement element, int index) {
+        try {
+            if (element.isDisplayed()) {
+                Select dropdown = new Select(element);
+                dropdown.selectByIndex(index);
+                System.out.println("Dropdown value selected by index successfully.");
+            } else {
+                System.out.println("Dropdown is not interactable.");
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+
+    public static void selectDropdownByValue(WebElement element, String value) {
+        try {
+            if (element.isDisplayed()) {
+                Select dropdown = new Select(element);
+                dropdown.selectByValue(value);
+                System.out.println("Dropdown value selected by value successfully.");
+            } else {
+                System.out.println("Dropdown is not interactable.");
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/utils/Webdriver.java
+++ src/main/java/utils/Webdriver.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 import java.io.IOException;
 
-public class Webdriver extends Property{
-    WebDriver driver;
+public class Webdriver extends Property {
+    protected WebDriver driver;
 
     public void getWebDriver() throws IOException {
         if(getProperty("config", "browserName").equalsIgnoreCase("chrome")) {
@@ -17,6 +17,12 @@ public class Webdriver extends Property{
             System.setProperty("webdriver.edge.driver", "src/main/resources/msedgedriver.exe");
             driver = new EdgeDriver();
         }
+        System.out.println("Browser launched is: " + getProperty("config", "browserName"));
+    }
+
+    public void launchBrowser() throws IOException {
         driver.get(getProperty("config", "url"));
+        driver.manage().window().maximize();
+        System.out.println("Launched URL: " + getProperty("config", "url"));
     }
 }
